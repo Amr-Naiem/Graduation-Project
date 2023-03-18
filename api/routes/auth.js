04b773d3ec6f54e06734231d5ac6b6d9ecd29fa5
Amr-Naiem <amrnaeim512@gmail.com>
2023-03-18 05:18:50 +0200

    fixed the login problem

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -25,13 +25,18 @@ router.post("/login", async (req, res) => {
   try {
 
     const user = await User.findOne({ username: req.body.username });
-    !user && res.status(400).json("Wrong credentials!");
-    
     const validated = await bcrypt.compare(req.body.password, user.password);
-    !validated && res.status(400).json("Wrong credentials!");
-
-    const { password, ...others } = user._doc;
+    if(!user){
+      res.status(400).json("Wrong credentials!")
+    }
+    else if (!validated){
+      res.status(400).json("Wrong credentials!")
+    }
+    else {
+      const { password, ...others } = user._doc;
     res.status(200).json(password);
+    }
+    
   } catch (err) {
     res.status(500).json(err);
   }
